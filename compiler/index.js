64f9a9a7c79190814a701e9b4108420509b5d1ab
Ducklett <ivy16203@gmail.com>
2022-05-26 10:50:32 +0200

    update index so it displays errors

--- compiler/index.js
+++ compiler/index.js
@@ -5,15 +5,21 @@ const { bind } = require('./binder')
 const { lower } = require('./lowerer')
 const { emitAsm } = require('./emitter')
 const { outputHtml } = require('./tool/outputHtml')
+const { hasErrors: hasErrors, displayErrors } = require('./errors')
 
-compile('examples/10_snake.x3')
+// compile('examples/09_rtti.x3')
+// compile('examples/10_snake.x3')
+compile('examples/11_enum.x3')
 // compile('examples/coreutils/test.x3')
 
 function compile(filename) {
 	const file = ({ path: filename, code: read(filename) })
 	const syntaxTree = parse(file)
+	if (hasErrors()) displayErrors()
 
 	const ast = bind(syntaxTree)
+	if (hasErrors()) displayErrors()
+
 	const [loweredAst, meta] = lower(ast)
 	const entrypoint = meta.entrypoint?.name || 'main'
 	emitAsm(loweredAst, { entrypoint })
